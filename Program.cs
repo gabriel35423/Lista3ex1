9a663d277bdd00cb8e19eca62f60e6da2ed0bd78
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-06-21 21:23:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 0;
+
+            do
+            {
+                Console.WriteLine("Informe o Valor:");
+                valor = double.Parse(Console.ReadLine());
+            }
+            while (valor < 0);
+            Console.WriteLine("Valor: {0} Aceito!", valor);
+        }
+    }
+}
